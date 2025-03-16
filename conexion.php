16b7d71537a7b588b0d2c19d6a1d018727eb5beb
BraYanx0258 <brayanbragan1350@gmail.com>
2025-03-16 16:11:34 -0500

    agrego conexion
    5yg5yg

---
+++ conexion.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$user = "root"; 
+$password = ""; 
+$database = "blog_deportes-main"; 
+
+$conn = new mysqli($host, $user, $password, $database);
+
+// Verificar conexión
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
+}
+?>
