9402927c83224b54e511f3d2e080564afbc8d554
BraYanx0258 <brayanbragan1350@gmail.com>
2025-03-19 12:26:28 -0500

    jjikjk
    jk

--- php/conexion.php
+++ php/conexion.php
@@ -1,11 +1,11 @@
 <?php
-$servername = "localhost"; 
-$username = "root";        
-$password = "";            
-$dbname = "blog_deportes-main"; 
+$servername = "localhost"; // Servidor de la base de datos
+$username = "root";        // Nombre de usuario de MySQL
+$password = "";            // Contraseña de MySQL
+$dbname = "nombre_de_tu_base_de_datos"; // Nombre de la base de datos
 
 // Crear conexión
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname , $db);
 
 // Verificar conexión
 if ($conn->connect_error) {
