02e674adbf5590ae5211cd26b3c2740685a1d255
brayan <brayanbragan1350@gmail.com>
2025-03-19 15:46:00 -0500

    cambia a mi username

--- php/conexion.php
+++ php/conexion.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost"; // Servidor de la base de datos
-$username = "root";        // Nombre de usuario de MySQL
-$password = "";            // Contraseña de MySQL
-$dbname = "blog_deportes-main"; // Nombre de la base de datos
+$servername = "localhost"; 
+$username = "root";        
+$password = 123456;            
+$dbname = "blog_deportes-main"; 
 
 // Crear conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
