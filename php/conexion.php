9ffc7090290912554160f61ce8c303540e8260a1
BraYanx0258 <brayanbragan1350@gmail.com>
2025-03-19 12:31:44 -0500

    listo
    correcciones requeridas

--- php/conexion.php
+++ php/conexion.php
@@ -2,11 +2,10 @@
 $servername = "localhost"; // Servidor de la base de datos
 $username = "root";        // Nombre de usuario de MySQL
 $password = "";            // Contraseña de MySQL
-$dbname = "nombre_de_tu_base_de_datos"; // Nombre de la base de datos
+$dbname = "blog_deportes-main"; // Nombre de la base de datos
 
 // Crear conexión
-$conn = new mysqli($servername, $username, $password, $dbname , $db);
-
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Verificar conexión
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
