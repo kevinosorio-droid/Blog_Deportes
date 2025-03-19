4b1c06686dcc92bb1d4f8ac42232ac30aa35a190
BraYanx0258 <brayanbragan1350@gmail.com>
2025-03-19 12:18:36 -0500

    update conexion
    organice la conexxion del proyecto

--- php/conexion.php
+++ php/conexion.php
@@ -1,14 +1,14 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "blog_deportes-main";
+$servername = "localhost"; 
+$username = "root";        
+$password = "";            
+$dbname = "blog_deportes-main"; 
 
 // Crear conexión
-$db = mysqli_connect($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Verificar conexión
-if (!$db) {
-    die("Conexión fallida: " . mysqli_connect_error());
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
 }
 ?>
